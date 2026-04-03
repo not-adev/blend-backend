1bfb261839d382bd9c470a29819ccc3e254513ec
not-adev <rcduaru2718@gmail.com>
2026-04-03 16:42:29 +0530

    socket manger funtion defination changed

--- src/socket/socket.manager.js
+++ src/socket/socket.manager.js
@@ -1,16 +1,16 @@
 import * as roomController from './socket.controlers/socket.controler.groupCrud'
 export default function registerSocketHandlers(io, socket) {
 
-  socket.on("room:create", (data) =>
-    roomController.createRoom(io, socket, data)
+  socket.on("room:create",async (data) =>
+    await roomController.createRoom(io, socket, data)
   );
 
-  socket.on("room:join", (data) =>
-    roomController.joinRoom(io, socket, data)
+  socket.on("room:join", async (data) =>
+   await roomController.joinRoom(io, socket, data)
   );
 
-  socket.on("room:leave", (data) =>
-    roomController.leaveRoom(io, socket, data)
+  socket.on("room:leave", async (data) =>
+   await roomController.leaveRoom(io, socket, data)
   );
 
 }
