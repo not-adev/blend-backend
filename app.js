3c7b2bca70801be9ba526d1105c753cd18ca52c3
not-adev <rcduaru2718@gmail.com>
2026-04-18 15:05:55 +0530

    added cors to all for temp in backend

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors';
 import http from 'http'
 import { Server } from 'socket.io'
 import { clerkMiddleware } from "@clerk/express";
@@ -13,10 +14,9 @@ import { groupJoinRoutes } from './src/routes/group/group.join.js';
 import { userAuthRouter } from './src/routes/user/userAuth.js';
 const app = express()
 const port = 3000
-
-
 const server = http.createServer(app)
 const io = new Server(server)
+app.use(cors())
 app.use(clerkMiddleware())
 app.use(express.json())
 app.use((req, res, next) => {
