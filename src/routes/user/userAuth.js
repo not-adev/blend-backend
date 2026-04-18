29ddb483dae7015c2e8f25f15997178c8ae4b178
not-adev <rcduaru2718@gmail.com>
2026-04-18 15:02:17 +0530

    added use auth rout

--- src/routes/user/userAuth.js
+++ src/routes/user/userAuth.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import * as userAuthControler from '../../controlers/user.controlers/user.controler.auth.js'
 export const userAuthRouter = Router()
-userAuthRouter.get('/' , userAuthControler.syncUserControler)
+userAuthRouter.post('/' , userAuthControler.syncUserControler)
 
 
 
